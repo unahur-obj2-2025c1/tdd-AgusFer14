77a3e6d81528f925a994254cdf3cf10b86b816da
AgusFer14 <fernandesagustin@yahoo.com>
2025-04-14 20:31:32 -0300

    Resolucion de trabajo

--- src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
@@ -1,5 +1,40 @@
 package ar.edu.unahur.obj2.tdd;
 
 public class CajaFuerte {
-
+	
+	private Boolean abierta;
+	
+	private Integer clave;
+	
+	private Boolean bloqueada = false;
+	
+	private Integer intentosFallidos = 0;
+	
+	public CajaFuerte() {
+		this.abierta = true;
+	}
+	
+	public Boolean estaAbierta() {
+		return abierta;
+	}
+	
+	public void cerrar(Integer clave) {
+		if(this.abierta) {
+			this.clave = clave;
+			this.abierta = false;
+		}
+	}
+	
+	public void abrir(Integer clave) {
+		if(this.clave.equals(clave) && !this.estaBloqueada()) {
+			this.abierta = true;
+			this.intentosFallidos = 0;
+		} else {
+			this.intentosFallidos++;
+		}
+	}
+	
+	public Boolean estaBloqueada() {
+		return this.intentosFallidos >= 3;
+	}
 }
